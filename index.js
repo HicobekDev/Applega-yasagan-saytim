aab1745914680375825b3e077d7de3a33a5869b2
HicobekDev <93650395+HicobekDev@users.noreply.github.com>
2021-11-06 13:18:10 +0500

    DATA LEARNING CENTER DATA KIDS
    DATA LEARNING CENTER DATA KIDS

---
+++ index.js
@@ -0,0 +1,9 @@
+prompt("1-sonni kiriting");
+prompt("Ismingizni kiriting");
+prompt("Parolni kiriting");
+confirm("Welcemo to Apple");
+console.log(50 / 50);
+let age = 7;
+age = 4;
+console.log(age);
+
